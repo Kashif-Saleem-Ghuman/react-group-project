96541ac3ea1bee87a0401491a286d71ea267a888
Teklay <teklay@gmail.com>
2023-01-30 23:06:38 +0300

    Add empty website header

---
+++ src/component/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../logo.png';
+import '../index.css';
+
+const Nav = () => (
+  <nav className="navbar">
+    <div className="logo">
+      <img src={logo} alt="logo" />
+      <h1>Space Travelers Hub</h1>
+    </div>
+    <ul className="nav-links">
+      <li><NavLink to="/" exact className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Rockets</NavLink></li>
+      <li><NavLink to="/missions" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Missions</NavLink></li>
+      <li className="myprofile"><NavLink to="/myprofile" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>My Profile</NavLink></li>
+    </ul>
+  </nav>
+);
+
+export default Nav;
